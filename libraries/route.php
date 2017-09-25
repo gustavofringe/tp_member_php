972e8ff9875317855d1130d177391872bd38e255
Guillaume <dussartguillaume@orange.fr>
2017-09-25 21:00:15 +0200

    add new route

--- libraries/route.php
+++ libraries/route.php
@@ -1,11 +1,23 @@
 <?php
 class Route
 {
+    public $request
     /**
      * Route constructor.
      */
     public function __construct()
-    {
+    { 
+		$controller = $this->loadController();
+		$action = $this->request->action;
+		if($this->request->prefix){
+			$action = $this->request->prefix.'_'.$action;
+		}
+		if(!in_array($action , array_diff(get_class_methods($controller),get_class_methods('Controller'))) ){
+			$this->error('Le controller '.$this->request->controller.' n\'a pas de méthode '.$action); 
+		}
+		call_user_func_array(array($controller,$action),$this->request->params); 
+        $controller->render($action);
+        //
         $url = isset($_GET['url']) ? $_GET['url'] : null;
         $url = rtrim($url, "'");
         $url = explode("/", $url);
@@ -38,5 +50,16 @@ class Route
             die();
         }
         $controller->{$url[0]}();
+       
+    }
+    function loadController(){
+        $name = ucfirst($this->request->controller).'Controller'; 
+        $file = ROOT.DS.'controller'.DS.$name.'.php';
+        if(!file_exists($file)){
+            $this->error('Le controller '.$this->request->controller.' n\'existe pas'); 
+        } 
+        require $file; 
+        $controller = new $name($this->request); 
+        return $controller;  
     }
 }
